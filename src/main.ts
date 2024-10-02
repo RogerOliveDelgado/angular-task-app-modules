19f0a5085cce2d24fff76963e9388d69f6461e0f
r.olive <r.olive@plugchain.energy>
2024-10-02 10:53:18 +0200

    Feat: Add modules to app

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
 
-import { AppComponent } from './app/app.component';
+//This function renders component, only if AppComponent is a standalone component
+// bootstrapApplication(AppComponent).catch((err) => console.error(err));
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule);
